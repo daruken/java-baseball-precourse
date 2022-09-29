44d0238f0e525f9e885982c9f6ac8a2d027ab029
daruken <>
2022-09-29 17:16:05 +0900

    feat: Player 모델 숫자 입력 받는 메소드 추가

--- src/main/java/baseball/model/player/Player.java
+++ src/main/java/baseball/model/player/Player.java
@@ -1,7 +1,25 @@
 package baseball.model.player;
 
-public class Player {
-    public void inputNumber() {
+import baseball.contract.Config;
+import camp.nextstep.edu.missionutils.Console;
 
+public class Player {
+    public String inputNumber() {
+        String input = Console.readLine();
+        if (!isAllowNumber(input) || input.length() != Config.NUMBER_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+
+        return input;
+    }
+
+    private boolean isAllowNumber(String input) {
+        try {
+            Double.parseDouble(input);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+
+        return true;
     }
 }
