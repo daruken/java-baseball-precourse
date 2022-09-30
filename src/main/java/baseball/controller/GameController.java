249689efae95b37f9a01204b79216203dc1c7816
daruken <>
2022-09-30 17:20:03 +0900

    feat: GameController 비즈니스 로직 개발 - Game, Player(사용자), Computer(컴퓨터) 3가지 도메인 모델을 사용하여 숫자야구게임을 제어   - Player에게 입력 받은 숫자를 Computer가 생성한 숫자(3자리)와 비교     - 3자리가 모두 일치하면 승리     - 3자리가 일치하지 않다면, 힌트로 주는 결과 볼 또는 낫싱을 활용해서 계속 입력 받아서 다시 비교   - 승리하면 다시하기(1) or 종료(2) 선택을 할 수 있도록 제공

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,31 +1,56 @@
 package baseball.controller;
 
 import baseball.contract.Message;
+import baseball.model.computer.Computer;
 import baseball.model.game.Game;
+import baseball.model.player.Player;
+
+import static baseball.contract.Config.BALL_NUMBER_LENGTH;
 
 public class GameController {
     private final Game game = new Game();
+    private Computer computer = new Computer();
+    private final Player player = new Player();
 
     public void play() {
         do {
-            this.playGame();
+            playGame();
         } while (game.isRunning);
     }
 
     public void playGame() {
-        System.out.println(Message.PLAY);
-        String input = game.play();
-        System.out.println("input is : " + input);
-        
+        System.out.print(Message.PLAY);
+        String playerInput = player.inputNum();
+
+        String result = game.compareBall(playerInput, computer.getBall());
+        System.out.println(result);
+        if (result.equals(BALL_NUMBER_LENGTH + Message.STRIKE)) {
+            System.out.println(Message.MATCH);
+            endGame();
+        }
     }
 
     public void endGame() {
         game.setRunning(false);
-        System.out.println(Message.END);
+
         System.out.println(Message.REPLAY);
+        selectMenuGame();
     }
 
-    public void terminateGame() {
-        System.exit(0);
+    public void selectMenuGame() {
+        String result = player.selectMenu();
+        if (result.equals("1")) {
+            this.replayGame();
+        }
+
+        if (result.equals("2")) {
+            System.out.println(Message.END);
+        }
+    }
+
+    public void replayGame() {
+        game.setRunning(true);
+        this.computer = new Computer();
+        this.play();
     }
 }
