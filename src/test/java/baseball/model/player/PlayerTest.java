023ac6fead03dedd445af2a761022f269a6e4787
daruken <>
2022-09-30 16:45:04 +0900

    test: REPLAY 메뉴 선택 때 입력받는 숫자도 예외 처리 위해 파라미터 추가

--- src/test/java/baseball/model/player/PlayerTest.java
+++ src/test/java/baseball/model/player/PlayerTest.java
@@ -2,6 +2,8 @@ package baseball.model.player;
 
 import org.junit.jupiter.api.Test;
 
+import static baseball.contract.Config.BALL_NUMBER_LENGTH;
+import static baseball.contract.Config.SELECT_MENU_LENGTH;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -23,11 +25,20 @@ class PlayerTest {
     }
 
     @Test
-    void 사용자_입력값_체크_사이즈초과() {
+    void 사용자_볼_입력값_체크_사이즈초과() {
         Player player = new Player();
 
         assertThatThrownBy(() -> {
-            player.isValidateSizeNumber("1234");
+            player.isValidateSizeNumber(BALL_NUMBER_LENGTH, "1234");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 사용자_메뉴_입력값_체크_사이즈초과() {
+        Player player = new Player();
+
+        assertThatThrownBy(() -> {
+            player.isValidateSizeNumber(SELECT_MENU_LENGTH, "12");
         }).isInstanceOf(IllegalArgumentException.class);
     }
 }
